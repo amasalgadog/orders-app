dae02e475cd62e97cfcdf29d06d6d8a75912ba79
gato4d <alvaro.asalgado@ug.uchile.cl>
2025-07-10 21:16:38 -0400

    added DiscountService.java + changes in OrderService class in OrderService.java

--- src/main/java/com/ejemplo/OrderService.java
+++ src/main/java/com/ejemplo/OrderService.java
@@ -1,7 +1,12 @@
 package com.ejemplo;
 
 public class OrderService {
-    public double getTotal(double subtotal, boolean hasDiscount, boolean hasExpressShipment){
+    private DiscountService discountService;
+    public OrderService(DiscountService discountService) {
+        this.discountService = discountService;
+    }
+
+    public double getTotal(double subtotal, String discountCode, boolean hasExpressShipment){
         // sin esta parte del código, el test de OrderServiceTest.java falla
         // este el error que se produce:
         // OrderServiceTest.testNegativeSubtotal:50 Expected java.lang.IllegalArgumentException to be thrown, but nothing was thrown.
@@ -11,7 +16,7 @@ public class OrderService {
         }
         // Fin de la parte del código que evita el error en el test
 
-        double discount = hasDiscount ? 0.10 : 0;
+        double discount = discountService.getRate(discountCode);
         double shipment = hasExpressShipment ? 20.0 : 10.0;
         return (subtotal * (1 - discount)) + shipment;
     }
